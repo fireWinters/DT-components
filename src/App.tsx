f0d1d04a1615eb7fb745cca61bc29c6b00a00031
Diana Tang <1987567948@qq.com>
2024-11-03 23:55:36 +0800

    feat: add initial App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2024-11-03 22:00:02
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /DT-components/src/App.tsx
+ */
+function App() {
+    return (
+      
+        <div className="App">
+         
+        </div>
+     
+    );
+  }
+  
+  export default App;
