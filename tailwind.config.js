3f2e61252f1eae5d38479e948f247018fb1178a5
Diana Tang <1987567948@qq.com>
2024-11-03 21:14:40 +0800

    feat: install @tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2024-11-03 20:47:21
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /DT-components/tailwind.config.js
+ */
 /** @type {import('tailwindcss').Config} */
+const typegraphy=require('@tailwindcss/typography')
 module.exports = {
-  content: [],
+  content: ['./src/**/*.tsx', './src/**/*.html', './public/index.html'],
+  important: '#root',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [typography
+    // , require('./plugin')
+    // 后续处理一下自己的plugin
+  ],
 }
 
