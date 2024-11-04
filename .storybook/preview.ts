a3a56e69d3baa637228a9cbe7c7235122d24b588
Diana Tang <1987567948@qq.com>
2024-11-04 23:33:50 +0800

    feat:要疯了，给我删了。。。气死

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,26 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2024-11-04 19:11:25
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /DT-components/.storybook/preview.ts
+ */
 import type { Preview } from "@storybook/react";
+import { withThemeByClassName } from '@storybook/addon-themes';
+import '../src/App.css'; //里面包含tailwindcss的设置
+
+/* snipped for brevity */
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+    attributeName: 'data-mode',
+  }),
+];
 
 const preview: Preview = {
   parameters: {
